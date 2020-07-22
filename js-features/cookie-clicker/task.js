9edc36a76ef2c79418b9368aba8733c68cb91e98
Dmitry <mardarovsky-d@yandex.ru>
2020-07-22 19:14:18 +0300

    cookie-clicker - click speed added

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,4 +1,6 @@
-let clickCookie = document.getElementById('cookie');
+const clickCookie = document.getElementById('cookie');
+let clickSpeed = document.getElementById('click__speed'),
+    timeArray = [0, 0];
 
 clickCookie.onclick = function () {
     let clickCount = document.getElementById('clicker__counter');
@@ -7,5 +9,9 @@ clickCookie.onclick = function () {
         clickCookie.width = 150;
     } else {
         clickCookie.width = 200;
-    }
+    };
+    let clickTime = new Date();
+    timeArray.push(clickTime.getTime());
+    timeArray.shift(0);
+    clickSpeed.textContent = 1/((timeArray[1] - timeArray[0])/1000);
 }
