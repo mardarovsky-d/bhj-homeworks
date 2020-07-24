95b3176c25915c2218ee1a715c74749749cd7b97
Dmitry <mardarovsky-d@yandex.ru>
2020-07-24 17:40:27 +0300

    clearInterval added

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,5 +1,5 @@
 let timeDecrement = function () {
     let timerInt = document.getElementById('timer');
-    timerInt.textContent == 0 ? alert('Вы победили в конкурсе!') : timerInt.textContent--;
+    timerInt.textContent == 0 ? alert('Вы победили в конкурсе!') || clearInterval(timerID) : timerInt.textContent--;
 }
-setInterval(timeDecrement, 1000);
+let timerID = setInterval(timeDecrement, 1000);
