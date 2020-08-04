cd217b8bd825a60435988c127d35d8c55f7aec49
Dmitry <mardarovsky-d@yandex.ru>
2020-08-04 14:41:30 +0300

    поменял тернарный оператор на if и добавил clearInterval в условие

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,5 +1,10 @@
 let timeDecrement = function () {
     let timerInt = document.getElementById('timer');
-    timerInt.textContent == 0 ? alert('Вы победили в конкурсе!') || clearInterval(timerID) : timerInt.textContent--;
+    if (timerInt.textContent == 0) {
+        clearInterval(timerID);
+        alert('Вы победили в конкурсе!');
+    } else {
+        timerInt.textContent--;
+    }
 }
 let timerID = setInterval(timeDecrement, 1000);
