8c737b0be600c1bf67bcf7e5e02736563a4c5b3a
Dmitry <mardarovsky-d@yandex.ru>
2020-08-18 17:32:54 +0300

    Todo is done

---
+++ document-structure/todo/task.js
@@ -0,0 +1,26 @@
+const input = document.getElementById('task__input');
+const addTaskButton = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+
+addTaskButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (input.value != '') {
+        tasksList.insertAdjacentHTML('beforeend', 
+        `<div class="task">
+            <div class="task__title">
+                ${input.value}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>`
+        );
+        input.value = '';
+        const tasks = Array.from(document.querySelectorAll('.task'));
+        tasks.forEach((task) => {
+            const removeTaskButton = task.querySelector('.task__remove');
+            removeTaskButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                task.remove();
+            })
+        })
+    }
+});
