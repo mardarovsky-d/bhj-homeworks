95f6dfe7aa1501c9592860d3b9016fb102f9738c
Dmitry <mardarovsky-d@yandex.ru>
2020-08-21 14:05:35 +0300

    Progressbar fixed

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,8 +4,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const xhr = new XMLHttpRequest();
     const file = document.getElementById('file');
-    const formData = new FormData();
-    formData.append('file', file);
+    const formData = new FormData(form);
     xhr.upload.onprogress = function (event) {
         const progress = document.getElementById('progress');
         progress.value = (event.loaded / event.total).toFixed(3);
